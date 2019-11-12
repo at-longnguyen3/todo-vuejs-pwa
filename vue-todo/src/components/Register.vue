10a7b42c6bc617250810c4ba4a5566107db8b6f1
at-longnguyen3 <long.nguyen3@asiantech.vn>
2019-11-12 11:55:41 +0700

    Edit erorr text

--- vue-todo/src/components/Register.vue
+++ vue-todo/src/components/Register.vue
@@ -18,7 +18,7 @@
               <input class="input100" type="password" name="pass" v-model="confirmPassword" placeholder="Confirm Password">
               <span class="focus-input100"></span>
             </div>
-             <p class="error" v-if="checkPass">The password and confirm password are not the same</p>
+             <p class="error" v-if="checkPass">The password and confirm password must be the same</p>
             <div class="container-login100-form-btn flex-col-c">
               <button type="button" class="login100-form-btn" @click="signUp()" :disabled="checkPass">Submit</button>
               <router-link class="login100-form-btn" to="/login">Sign in</router-link>
